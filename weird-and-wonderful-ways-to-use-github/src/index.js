e47a2e94d9ceaa1e64fc668c73e60452e5d0f80e
David Wesst <david@cocobokostudios.com>
2021-12-21 13:05:38 -0600

    build webpack scaffold for creating slides

---
+++ weird-and-wonderful-ways-to-use-github/src/index.js
@@ -0,0 +1,7 @@
+import Reveal from 'reveal.js'
+import 'reveal.js/dist/reveal.css'
+import 'reveal.js/dist/theme/black.css'
+
+console.log('Hello Webpack Again!');
+Reveal.initialize();
+
---
+++ weird-and-wonderful-ways-to-use-github/webpack.config.js
@@ -0,0 +1,42 @@
+const path = require('path');
+
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+
+module.exports = {
+    mode: 'development',
+    resolve: {
+        modules: ['node_modules']
+    },
+    entry: './src/index.js',
+    output: {
+        filename: 'index.js',
+        path: path.resolve(__dirname, '/dist')
+    },
+    watch: true,
+    watchOptions: {
+        ignored: '**/node_modules/'
+    },
+    module: {
+        rules: [
+          {
+            test: /\.(ttf|woff|eot)$/,
+            loader: 'file-loader',
+            options: {
+              name: '[name].[ext]?[hash]'
+            }
+          },
+          {
+            test: /\.css$/,
+            use:['style-loader','css-loader']
+          }
+        ],
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: path.resolve(__dirname, "src", "index.html")
+        })
+    ],
+    devServer: {
+        hot: true
+    }
+}
